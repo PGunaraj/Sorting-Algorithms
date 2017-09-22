7478056906fba5151644310f0f486e808d8ab0fd
PRIYADHARSHINI GUNARAJ <pgunaraj@asu.edu>
2017-09-21 18:16:43 -0700

    Update InsertionSort.py

--- InsertionSort.py
+++ InsertionSort.py
@@ -10,3 +10,16 @@ def insertionSort(li):
 alist = [54,26,93,17,77,31,44,55,20]
 insertionSort(alist)
 print(alist)
+
+
+#optimized code
+def insertionSort(li):
+    for i in range(1,len(li)):
+        pos=i
+        while pos>0 and li[pos]<li[pos-1]:
+            li[pos],li[pos-1]=li[pos-1],li[pos]
+            pos=pos-1
+
+alist = [54,26,93,17,77,31,44,55,20]
+insertionSort(alist)
+print(alist)
